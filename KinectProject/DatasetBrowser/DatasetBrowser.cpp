0aa8e45ca420233c680ce166b24fcbaa44133508
Jose Padilla <sargepl@gmail.com>
2014-07-21 16:54:51 +0100

    Remove window centering code

--- KinectProject/DatasetBrowser/DatasetBrowser.cpp
+++ KinectProject/DatasetBrowser/DatasetBrowser.cpp
@@ -6,7 +6,6 @@
 #include "viewer/InstanceViewerWindow.h"
 #include "InstanceWidgetItem.h"
 #include "DatasetSelector.h"
-#include <QtWidgets/QDesktopWidget>
 #include <QTimer>
 #include <QMessageBox>
 
@@ -35,25 +34,6 @@ DatasetBrowser::DatasetBrowser(QWidget *parent) :
     connect(ui->listSamples, SIGNAL(itemChanged(QListWidgetItem*)), this, SLOT(listItemChange(QListWidgetItem*)));
     connect(ui->listInstances, SIGNAL(itemActivated(QListWidgetItem*)), this, SLOT(instanceItemActivated(QListWidgetItem*)));
     connect(ui->comboType, SIGNAL(currentIndexChanged(int)), this, SLOT(comboBoxChange(int)));
-
-    // Show this window centered
-    QDesktopWidget *desktop = QApplication::desktop();
-    int screenWidth, width;
-    int screenHeight, height;
-    int x, y;
-    QSize windowSize;
-
-    screenWidth = desktop->availableGeometry(desktop->primaryScreen()).width();
-    screenHeight = desktop->availableGeometry(desktop->primaryScreen()).height();
-    windowSize = size();
-    width = windowSize.width();
-    height = windowSize.height();
-
-    x = (screenWidth - width) / 2;
-    y = (screenHeight - height) / 2;
-    y -= 50;
-
-    move ( x, y );
 }
 
 DatasetBrowser::~DatasetBrowser()
